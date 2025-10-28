7a8751dccb7a758601f06e099a32eaa093038d3c
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-10-28 15:39:37 -0400

    Add utility for getting extra args

--- hatch_build/cli.py
+++ hatch_build/cli.py
@@ -1,10 +1,21 @@
-import argparse
+from argparse import ArgumentParser
+from typing import Callable, List, Optional, Tuple
 
 from hatchling.cli.build import build_command
 
+__all__ = (
+    "hatchling",
+    "parse_extra_args",
+)
+_extras = None
 
-def hatchling() -> int:
-    parser = argparse.ArgumentParser(prog="hatch-build", allow_abbrev=False)
+
+def parse_extra_args(subparser: Optional[ArgumentParser]) -> List[str]:
+    return subparser.parse_args(_extras) if _extras else {}, []
+
+
+def _hatchling_internal() -> Tuple[Optional[Callable], Optional[dict], List[str]]:
+    parser = ArgumentParser(prog="hatch-build", allow_abbrev=False)
     subparsers = parser.add_subparsers()
 
     defaults = {"metavar": ""}
@@ -21,7 +32,7 @@ def hatchling() -> int:
     # but they must be after a '--' separator
     if extras and extras[0] != "--":
         parser.print_help()
-        return 1
+        return None, None, None
 
     # Wrap the parsed arguments in a dictionary
     kwargs = vars(kwargs)
@@ -30,7 +41,19 @@ def hatchling() -> int:
         command = kwargs.pop("func")
     except KeyError:
         parser.print_help()
-    else:
-        command(**kwargs)
+        return None, None, None
+    return command, kwargs, extras[1:]  # Remove the '--' separator
 
+
+def hatchling() -> int:
+    global _extras
+
+    command, kwargs, extras = _hatchling_internal()
+    if command is None:
+        return 1
+
+    # Set so plugins can reference
+    _extras = extras
+
+    command(**kwargs)
     return 0
